08a1ccc201c448adeb7818c5967e923dcb5c3d8b
z4rp <jw+git@ichwillne.info>
2022-02-04 21:22:28 +0100

    switch to require_once for template config and refactor error handling
    refactor output to be written to a file in output/ directory

--- index.php
+++ index.php
@@ -8,17 +8,31 @@ if ($argc <= 1) {
 }
 
 $template = $argv[1];
-$config = @include "templates/{$template}.conf.php";
-if (!$config) {
+
+if(!file_exists("templates/{$template}.conf.php")) {
     echo "Config file templates/{$template}.conf.php not found";
     exit(1);
 }
 
+$config = require_once "templates/{$template}.conf.php";
 $loader = new \Twig\Loader\FilesystemLoader('templates/');
 $twig = new \Twig\Environment($loader);
 
 try {
-    echo $twig->render("{$template}.twig", $config);
+    $render = $twig->render("{$template}.twig", $config);
 } catch (\Exception $e) {
     echo $e->getMessage();
+    exit(1);
 }
+
+if(!file_exists('output/')) {
+    if(!mkdir('output/', 0755)) {
+        echo 'Could not create output/ directory';
+        exit(1);
+    }
+}
+
+if(file_put_contents("output/{$template}.conf", $render) === false) {
+    echo "Could not write to output/{$template}.conf";
+    exit(1);
+};
