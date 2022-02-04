1d3ae7b451257cdf3de84e0788f6e120822cabc9
z4rp <jw+git@ichwillne.info>
2022-02-04 21:41:50 +0100

    remove requirement for template config add newlines to everything we echo echo output path at he end

--- index.php
+++ index.php
@@ -3,18 +3,24 @@
 require_once 'vendor/autoload.php';
 
 if ($argc <= 1) {
-    echo "Usage: {$argv[0]} template";
+    echo "Usage: {$argv[0]} template\n";
     exit(1);
 }
 
 $template = $argv[1];
 
 if(!file_exists("templates/{$template}.conf.php")) {
-    echo "Config file templates/{$template}.conf.php not found";
-    exit(1);
+    echo "Config file templates/{$template}.conf.php not found\n";
+    echo 'Make sure to set your variables inside the twig template!\n';
+    $config = [];
+} else {
+    $config = require_once "templates/{$template}.conf.php";
+    if(!is_array($config)) {
+        echo 'Config doesn\'t return an array\n';
+        echo 'Make sure to set your variables inside the twig template!\n';
+    }
 }
 
-$config = require_once "templates/{$template}.conf.php";
 $loader = new \Twig\Loader\FilesystemLoader('templates/');
 $twig = new \Twig\Environment($loader);
 
@@ -27,12 +33,14 @@ try {
 
 if(!file_exists('output/')) {
     if(!mkdir('output/', 0755)) {
-        echo 'Could not create output/ directory';
+        echo 'Could not create output/ directory\n';
         exit(1);
     }
 }
 
 if(file_put_contents("output/{$template}.conf", $render) === false) {
-    echo "Could not write to output/{$template}.conf";
+    echo "Could not write to output/{$template}.conf\n";
     exit(1);
 };
+
+echo "Output written to output/{$template}.conf\n";
