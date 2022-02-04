ccb0e2bde5a644ca5b2fe5fc751306c09cc936f0
z4rp <jw+git@ichwillne.info>
2022-02-04 19:05:58 +0100

    implement this simple project (what else should I put here lol) remove templates from .gitignore again add example template

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+if($argc <= 1) {
+    echo "Usage: {$argv[0]} template";
+    exit(1);
+}
+
+$template = $argv[1];
+$config = @include "templates/{$template}.conf.php";
+if(!$config) {
+    echo "Config file templates/{$template}.conf.php not found";
+    exit(1);
+}
+
+$loader = new \Twig\Loader\FilesystemLoader('templates/');
+$twig = new \Twig\Environment($loader);
+
+try {
+    echo $twig->render("{$template}.twig", $config);
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
